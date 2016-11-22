3237c8fb3b5cac18e364028d61cafa1c98df3bfb
Rajshree Tiwari <rajshree.tiwari@neosofttech.com>
2016-11-22 14:31:04 +0530

    My sample text commit

---
+++ app/src/main/java/gitsample/com/gitsample/MainActivity.java
@@ -0,0 +1,13 @@
+package gitsample.com.gitsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
